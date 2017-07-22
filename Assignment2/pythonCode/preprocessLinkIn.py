b0c4767ca4726125dfa63e43d507d040371969cd
Xuqianshawn <xuqianshawn@gmail.com>
2017-07-22 16:08:33 +0800

    Check in modified code with train and predict

--- Assignment2/pythonCode/preprocessLinkIn.py
+++ Assignment2/pythonCode/preprocessLinkIn.py
@@ -11,20 +11,19 @@ from LinkedIn import LinkedIn
 from os import walk
 import numpy
 class Train:
-   linkins=[]
-   interests=[]
+    linkins=[]
+        interests=[]
+            def __init__(self):
+                print("in init")
+                    
+                    def getFeature(self):
 
-   def __init__(self):
-     print("in init")
 
-   def getFeature(self):
-
-
-    vectorizer = CountVectorizer(analyzer = "word",   \
-                                 tokenizer = None,    \
-                                 preprocessor = None, \
-                                 stop_words = None,   \
-                                 max_features = 100)
+vectorizer = CountVectorizer(analyzer = "word",   \
+                             tokenizer = None,    \
+                             preprocessor = None, \
+                             stop_words = None,   \
+                             max_features = 20)
     #print(self.tweets)
     print('--start--')
     print(len(self.linkins))
@@ -32,55 +31,62 @@ class Train:
     train_data_features = train_data_features.toarray()
     print('--feature length--'+str(len(train_data_features)))
     with open("test.txt", "w") as text_file:
-     for i in range(0, len(train_data_features)):
-      #text_file.write(str(train_data_features[0]).replace('[', '').replace(']', '')+'\n')
-      if (i==(len(train_data_features)-1)):
-       text_file.write(self.learnString(str(train_data_features[i])))
-      else:
-       text_file.write(self.learnString(str(train_data_features[i]))+'\r\n')
-    print(train_data_features)
-    return (train_data_features)
+        for i in range(0, len(train_data_features)):
+            #text_file.write(str(train_data_features[0]).replace('[', '').replace(']', '')+'\n')
+            if (i==(len(train_data_features)-1)):
+                text_file.write(self.learnString(str(train_data_features[i])))
+                    else:
+                        text_file.write(self.learnString(str(train_data_features[i]))+'\r\n')
+                    print(train_data_features)
+return (train_data_features)
+    
+    
+    def learnString(self,str):
+        cleanedStr=str
+            cleanedStr=cleanedStr.replace('[ ','[').replace('[','').replace('  ',' ').replace(' ',',').replace('\r\n','').replace('\n','').replace(']','')
+                return cleanedStr
+                    def populateLinkedInText(self,folderName):
+counter=0
+    for (dirpath, dirnames, filenames) in walk(folderName):
+        counter=len(filenames)
+        print('total number'+str(counter))
+        for i in range(0, counter):
+            #get the tweet if, load tweet from json
+            if(filenames[i]=='.DS_Store' or filenames[i]=='U100gnd.txt'):
+                continue
+            with open(folderName+'/'+str(filenames[i])) as data_file:
+                data = data_file.read()
+                    
+                    print(data)
+                        #add tweet
+                        #print(row[1])
+                        linkedin=LinkedIn(data)
+                            self.linkins.append(linkedin.meaningfulText)
 
-
-   def learnString(self,str):
-       cleanedStr=str
-       cleanedStr=cleanedStr.replace('[ ','[').replace('[','').replace('  ',' ').replace(' ',',').replace('\r\n','').replace('\n','').replace(']','')
-       return cleanedStr
-   def populateLinkedInText(self,folderName, fromNum, toNumber):
-    counter=0
-
-    for i in range(fromNum, toNumber+1):
-
-        print('current number '+str(i))
-        with open(folderName+'/U'+str(i)+".txt") as data_file:
-         data = data_file.read()
-         linkedin=LinkedIn(data)
-        #print(self.st.stem(linkedin.meaningfulText))
-         self.linkins.append(linkedin.meaningfulText)
-
-    #print(all)
-   def populateInterests(self,path):
+print(all)
+    def populateInterests(self,path):
     with open(path) as data_file:
-     data = data_file.read()
-     lines = data.split('\n')
-     print(lines)
-     for i in range(0, len(lines)):
-      train.interests.append(lines[i])
-     print(train.interests)
+        data = data_file.read()
+            lines = data.split('\n')
+                print(lines)
+                    for i in range(0, len(lines)):
+                        train.interests.append(lines[i])
+                            print(train.interests)
 
 
 
-#nltk.download()
+
 train= Train()
-#for testing it should be train.populateLinkedInText('/Users/xuqianshawn/PycharmProjects/untitled/CS4242Plus/train/LinkedIn',421,570)
-train.populateLinkedInText('/Users/xuqianshawn/PycharmProjects/untitled/CS4242Plus/test/LinkedIn',421,570)
-
+train.populateLinkedInText('/Users/xuqianshawn/PycharmProjects/untitled/CS4242Plus/test/LinkedIn')
 feature=train.getFeature()
+forest = RandomForestClassifier( n_estimators = 100 )
+train.populateInterests('/Users/xuqianshawn/PycharmProjects/untitled/CS4242Plus/test/groundTruth.txt')
+forest = forest.fit(feature, train.interests)
+# s
+feature2=train.getFeature()
+result = forest.predict(feature2)
 
-
-#forest = RandomForestClassifier( n_estimators = 100 )
-#train.populateInterests('/Users/xuqianshawn/PycharmProjects/untitled/CS4242Plus/test/groundTruth.txt')
-#forest = forest.fit(feature, train.interests)
-#
-# feature2=train.getFeature()
-# result = forest.predict(feature2)
+# Write the test results
+#output = pd.DataFrame( data={"sentiment":result} )
+#output.to_csv( "Word2Vec_AverageVectors.csv", index=False, quoting=3 )
+#nltk.download()
